f6f9a07dabf0261bdf674908e774639abcc973c9
Aiji Uejima <aiji42@gmail.com>
2020-08-08 13:55:21 +0000

    Updates

---
+++ precache-manifest.3737e13a26209fce27d9fbcdc41f31ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cddbb13ea4804ee10d90b49c4e56ec5a",
+    "url": "/use-postal-jp/index.html"
+  },
+  {
+    "revision": "3e87287c5e5f43ac06bc",
+    "url": "/use-postal-jp/static/css/main.2c6a48fe.chunk.css"
+  },
+  {
+    "revision": "d6b9d8c929503b8c0ef9",
+    "url": "/use-postal-jp/static/js/2.c04b5a5f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/use-postal-jp/static/js/2.c04b5a5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e87287c5e5f43ac06bc",
+    "url": "/use-postal-jp/static/js/main.e219ce19.chunk.js"
+  },
+  {
+    "revision": "9de4dedb211d9ed6a1de",
+    "url": "/use-postal-jp/static/js/runtime-main.62fb7b5b.js"
+  }
+]);
